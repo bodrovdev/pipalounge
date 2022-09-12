f37e3b288997e2eb11333870348cc6e54eaf4f96
Igor Bodrov <bodrovigor02@gmail.com>
2022-09-12 13:21:19 +0300

    feat: 🎸 Добавил разметку, стилизацию

---
+++ src/js/libs/swiper.js
@@ -0,0 +1,10 @@
+import Swiper from 'swiper/bundle';
+
+//Слайдер из блока heading
+const heading__slider = new Swiper('.heading__slider', {
+  loop: true,
+  effect: "fade",
+  autoplay: {
+    delay: 4000,
+  },
+});
