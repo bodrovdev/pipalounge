be6d49d4d0e23e3f68537a16ae6e012f29c0786f
Igor Bodrov <bodrovigor02@gmail.com>
2022-09-16 13:37:02 +0300

    feat: 🎸 Добавил разметку, стилизацию

--- src/js/index.js
+++ src/js/index.js
@@ -7,27 +7,34 @@ let desc_buttons = document.querySelectorAll('.about__desc > div');
 
 desc_text.forEach((element) => {
   element.setAttribute('data-aos', 'fade-left');
-  element.setAttribute('data-aos-duration', '1500');
-  element.setAttribute('data-aos-delay', '5');
+  element.setAttribute('data-aos-duration', '1000');
+  element.setAttribute('data-aos-delay', '4');
 })
 
 desc_title.forEach((element) => {
   element.setAttribute('data-aos', 'fade-right');
-  element.setAttribute('data-aos-duration', '1500');
-  element.setAttribute('data-aos-delay', '5');
+  element.setAttribute('data-aos-duration', '1000');
+  element.setAttribute('data-aos-delay', '4');
 })
 
 desc_buttons.forEach((element) => {
   element.setAttribute('data-aos', 'fade-up');
-  element.setAttribute('data-aos-duration', '1500');
-  element.setAttribute('data-aos-delay', '5');
+  element.setAttribute('data-aos-duration', '1000');
+  element.setAttribute('data-aos-delay', '4');
 })
 
 //Анимация в блоке contacts
 let contacts_desc = document.getElementById('contacts_info');
 contacts_desc.setAttribute('data-aos', 'fade-right');
-contacts_desc.setAttribute('data-aos-duration', '1500');
-contacts_desc.setAttribute('data-aos-delay', '5');
+contacts_desc.setAttribute('data-aos-duration', '1000');
+contacts_desc.setAttribute('data-aos-delay', '4');
+
+//Смена положения блока с информацией в блоке contacts
+let contacts_desc_move = document.getElementById('contacts_info_move');
+contacts_desc_move.addEventListener('click', () => {
+  contacts_desc_move.classList.toggle('contacts__info-move--right');
+  contacts_desc.classList.toggle('contacts__info--right');
+})
 
 
 //Открытие и закрытие модального окна
